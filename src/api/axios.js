aa8e931ceb990e258019366c3212f5ed5141f7de
sakshisingh0101 <sakshiaingh0101>
2026-04-26 13:45:42 +0530

    axios backend url fixed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://tickflow-backend.onrender.com/api/v1',
   withCredentials: true, // For sending cookies with cross-origin requests
   headers: {
     'Content-Type': 'application/json',
